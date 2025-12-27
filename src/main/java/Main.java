b964b95689df87a8859b8370ef652bc145cb44b9
Bimsara-522 <bimsaratharuka522@gmail.com>
2025-12-27 21:35:05 +0530

    added Main class

---
+++ src/main/java/Main.java
@@ -0,0 +1,9 @@
+package main.java;
+
+public class Main {
+    public static void main(String args[]){
+        Calculator calculator = new Calculator();
+        int val = calculator.add(3, 6);
+        System.out.println(val);
+    }
+}
